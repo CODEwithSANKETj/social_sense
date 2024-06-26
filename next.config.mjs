fe2d4b7712704d4c3e2665fda960b3900d814e83
unknown <jaiswalsanket244@gmail.com>
2024-06-26 08:38:55 +0530

    Add Netlify configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash:true,
-    exportPathMap : async function (defaultpathmap){
-        return defaultpathmap
-    }
 };
 
 export default nextConfig;
